0af836e3033f977ef0e82819d367bf65c35219f5
Nycolas Valverde <nyckyvalverde@gmail.com>
2026-05-25 17:07:02 -0300

    feat: adiciona telas de cronometro, sessoes e perfil

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Sessoes from "./Sessoes";
+import Cronometro from "./Cronometro";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Sessoes />
+    <Cronometro />
   </StrictMode>
 );
 
