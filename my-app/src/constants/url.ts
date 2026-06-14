afede4cb487fbc1f9af8fa56c8a3be4f29dd76b2
Guilherme Visone <guivisone@gmail.com>
2026-06-14 19:19:17 -0300

    Arrumando banco de dados na web

--- my-app/src/constants/url.ts
+++ my-app/src/constants/url.ts
@@ -1,8 +1,8 @@
 //troque pelo IP do computador que roda o backend (server.js).
-const IP_DO_COMPUTADOR = '192.168.15.32';
+const IP_DO_COMPUTADOR = '192.168.15.129';
 const PORTA = 3000;
 
-export const API_URL = `http://192.168.0.200:3000`;
+export const API_URL = `http://192.168.15.129:3000`;
 
 export function getUrl(rota: string): string {
   const rotaNormalizada = rota.startsWith('/') ? rota : `/${rota}`;
