28fb3827352980a6441b26725e0a5fdfab0b8553
Gustavo <ghmsilva21@gmail.com>
2026-06-16 02:33:56 -0300

    converte campos DECIMAL do MySQL para number via typeCast

--- db.js
+++ db.js
@@ -3,11 +3,17 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',          //usuário MySQL
-    password: '2207',          //senha MySQL
-
+    password: 'mysqlGuz',          //senha MySQL
     database: 'nutri_esportiva',
     waitForConnections: true,
     connectionLimit: 10,
-})
+    typeCast(field, next) {
+        if (field.type === 'DECIMAL' || field.type === 'NEWDECIMAL') {
+            const value = field.string();
+            return value === null ? null : parseFloat(value);
+        }
+        return next();
+    },
+});
 
 module.exports = pool;
